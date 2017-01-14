46d2a75b5aa8accd7432228aa928caecebda55b8
mt <mt@DESKTOP-B50NA61>
2017-01-14 13:09:37 +0900

    テスト

---
+++ SwingDentaku/src/dentaku/dentaku.java
@@ -0,0 +1,16 @@
+package dentaku;
+
+import javax.swing.JFrame;
+
+public class dentaku {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		 JFrame frame = new JFrame("タイトル");
+		    frame.setBounds(100, 100, 200, 160);
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    frame.setVisible(true);
+
+	}
+}
